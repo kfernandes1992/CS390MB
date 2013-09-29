f152fb3420f5ab539e3a8877811d706887d993f9
Erik Risinger <erikrisinger@uma-vl905-128-119-29-181.wireless.umass.edu>
2013-09-29 14:15:41 -0400

    Added filter class

---
+++ CMPSCI390MB Accelerometer/FilterKME.m
@@ -0,0 +1,13 @@
+//
+//  FilterKME.m
+//  CMPSCI390MB Accelerometer
+//
+//  Created by Erik Risinger on 9/29/13.
+//  Copyright (c) 2013 Erik Risinger. All rights reserved.
+//
+
+#import "FilterKME.h"
+
+@implementation FilterKME
+
+@end
