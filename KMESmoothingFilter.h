dc36cd9ae848d113ae18b59f2d261974aed0fd4d
Erik Risinger <erikrisinger@uma-vl905-128-119-29-181.wireless.umass.edu>
2013-09-29 14:18:45 -0400

    Changed to specific filter class — Butterworth to be added later...

---
+++ KMESmoothingFilter.h
@@ -0,0 +1,13 @@
+//
+//  KMESmoothingFilter.h
+//  CMPSCI390MB Accelerometer
+//
+//  Created by Erik Risinger on 9/29/13.
+//  Copyright (c) 2013 Erik Risinger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KMESmoothingFilter : NSObject
+
+@end
