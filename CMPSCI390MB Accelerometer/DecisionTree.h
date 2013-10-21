1482d451bb6ba99afbaad6e1e1329a7b00fa85ef
Kevin Fernandes <User@1x-vl915-72-19-127-192.wireless.umass.edu>
2013-10-20 20:45:40 -0400

    DecisionTree.h & .m created.

---
+++ CMPSCI390MB Accelerometer/DecisionTree.h
@@ -0,0 +1,14 @@
+//
+//  DecisionTree.h
+//  CMPSCI390MB Accelerometer
+//
+//  Created by Kevin Fernandes on 10/20/13.
+//  Copyright (c) 2013 Erik Risinger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DecisionTree : NSObject
+
+-(NSString*) decideBasedOnValues:(NSArray*)values;
+@end
